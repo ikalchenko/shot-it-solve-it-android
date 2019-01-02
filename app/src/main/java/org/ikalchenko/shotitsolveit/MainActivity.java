9ac5a2740c523d215f792805c0969fcb22b64a22
Ilya Kalchenko <ilya.kalchenko@nixsolutions.com>
2019-01-02 16:14:34 +0200

    initial commit

---
+++ app/src/main/java/org/ikalchenko/shotitsolveit/MainActivity.java
@@ -0,0 +1,322 @@
+
+package org.ikalchenko.shotitsolveit;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.cameraview.CameraView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.StringJoiner;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public class MainActivity extends AppCompatActivity implements
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+
+    private static final String FRAGMENT_DIALOG = "dialog";
+
+    private static final int[] FLASH_OPTIONS = {
+            CameraView.FLASH_AUTO,
+            CameraView.FLASH_OFF,
+            CameraView.FLASH_ON,
+    };
+
+    private static final int[] FLASH_ICONS = {
+            R.drawable.ic_flash_auto,
+            R.drawable.ic_flash_off,
+            R.drawable.ic_flash_on,
+    };
+
+    private static final int[] FLASH_TITLES = {
+            R.string.flash_auto,
+            R.string.flash_off,
+            R.string.flash_on,
+    };
+
+    private int flashState;
+
+    private CameraView cameraView;
+
+    private Handler backgroundHandler;
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (cameraView != null) {
+                cameraView.takePicture();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraView = (CameraView) findViewById(R.id.camera);
+        if (cameraView != null) {
+            cameraView.addCallback(callback);
+        }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.take_picture);
+        if (fab != null) {
+            fab.setOnClickListener(onClickListener);
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            cameraView.start();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ConfirmationDialogFragment
+                    .newInstance(R.string.camera_permission_confirmation,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CAMERA_PERMISSION,
+                            R.string.camera_permission_not_granted)
+                    .show(getSupportFragmentManager(), FRAGMENT_DIALOG);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (backgroundHandler != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                backgroundHandler.getLooper().quitSafely();
+            } else {
+                backgroundHandler.getLooper().quit();
+            }
+            backgroundHandler = null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                if (permissions.length != 1 || grantResults.length != 1) {
+                    throw new RuntimeException("Error on requesting camera permission.");
+                }
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, R.string.camera_permission_not_granted,
+                            Toast.LENGTH_SHORT).show();
+                }
+                // No need to start camera here; it is handled by onResume
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.switch_flash:
+                if (cameraView != null) {
+                    flashState = (flashState + 1) % FLASH_OPTIONS.length;
+                    item.setTitle(FLASH_TITLES[flashState]);
+                    item.setIcon(FLASH_ICONS[flashState]);
+                    cameraView.setFlash(FLASH_OPTIONS[flashState]);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Handler getBackgroundHandler() {
+        if (backgroundHandler == null) {
+            HandlerThread thread = new HandlerThread("background");
+            thread.start();
+            backgroundHandler = new Handler(thread.getLooper());
+        }
+        return backgroundHandler;
+    }
+
+    private String predictNumbers(File photo) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://shot-it-solve-it.herokuapp.com/predict";
+
+        RequestBody formData = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", photo.getName(),
+                        RequestBody.create(MediaType.parse("text/plain"), photo))
+                .build();
+        Request request = new Request.Builder().url(url).post(formData).build();
+        Response response = client.newCall(request).execute();
+        String predictions = "";
+        try {
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            JSONArray infoJSONArray = jsonObject.getJSONArray("info");
+            StringJoiner joiner = new StringJoiner(", ");
+            for (int i = 0; i < infoJSONArray.length(); i++) {
+                joiner.add(infoJSONArray.getString(i));
+            }
+            predictions = joiner.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return predictions;
+    }
+
+
+    private CameraView.Callback callback = new CameraView.Callback() {
+
+        @Override
+        public void onPictureTaken(CameraView cameraView, final byte[] data) {
+            getBackgroundHandler().post(new Runnable() {
+                @Override
+                public void run() {
+                    File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                            "picture.jpg");
+                    OutputStream os = null;
+                    try {
+                        os = new FileOutputStream(file);
+                        os.write(data);
+                        os.close();
+                    } catch (IOException e) {
+                        Log.w(TAG, "Cannot write to " + file, e);
+                    } finally {
+                        if (os != null) {
+                            try {
+                                os.close();
+                            } catch (IOException e) {
+                                // Ignore
+                            }
+                        }
+                    }
+                    String predictions = "";
+                    try {
+                         predictions = predictNumbers(file);
+                    } catch (IOException e) {
+                        // Ignore
+                    }
+
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertBuilder.setMessage(predictions)
+                            .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = alertBuilder.create();
+                    alert.setTitle("Detected digits");
+                    alert.show();
+                }
+            });
+        }
+
+    };
+
+    public static class ConfirmationDialogFragment extends DialogFragment {
+
+        private static final String ARG_MESSAGE = "message";
+        private static final String ARG_PERMISSIONS = "permissions";
+        private static final String ARG_REQUEST_CODE = "request_code";
+        private static final String ARG_NOT_GRANTED_MESSAGE = "not_granted_message";
+
+        public static ConfirmationDialogFragment newInstance(@StringRes int message,
+                String[] permissions, int requestCode, @StringRes int notGrantedMessage) {
+            ConfirmationDialogFragment fragment = new ConfirmationDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_MESSAGE, message);
+            args.putStringArray(ARG_PERMISSIONS, permissions);
+            args.putInt(ARG_REQUEST_CODE, requestCode);
+            args.putInt(ARG_NOT_GRANTED_MESSAGE, notGrantedMessage);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Bundle args = getArguments();
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage(args.getInt(ARG_MESSAGE))
+                    .setPositiveButton(android.R.string.ok,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String[] permissions = args.getStringArray(ARG_PERMISSIONS);
+                                    if (permissions == null) {
+                                        throw new IllegalArgumentException();
+                                    }
+                                    ActivityCompat.requestPermissions(getActivity(),
+                                            permissions, args.getInt(ARG_REQUEST_CODE));
+                                }
+                            })
+                    .setNegativeButton(android.R.string.cancel,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getActivity(),
+                                            args.getInt(ARG_NOT_GRANTED_MESSAGE),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                    .create();
+        }
+    }
+}
